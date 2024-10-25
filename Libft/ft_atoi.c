e23ade8730b2eb7f6d72c90a760d6cb47c323f71
mhdd <em>
2024-10-25 16:38:24 +0100

    atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,34 @@
 /*   By: ahaddadi <ahaddadi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 19:46:07 by ahaddadi          #+#    #+#             */
-/*   Updated: 2024/10/23 20:08:17 by ahaddadi         ###   ########.fr       */
+/*   Updated: 2024/10/25 16:37:55 by ahaddadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_atoi(const char *str)
+{
+	int total = 0;
+	int sign = 1;
+	while(*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while(ft_isdigit(*str))
+		total = total * 10 + (*str++ - '0');
+	return (total * sign);
+}
+
+// #include <stdio.h>
+
+// int main()
+// {
+// 	printf("%d", ft_atoi("123"));
+// }
 
--- Libft/ft_isdigit.c
+++ Libft/ft_isdigit.c
@@ -6,13 +6,13 @@
 /*   By: ahaddadi <ahaddadi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 20:16:33 by ahaddadi          #+#    #+#             */
-/*   Updated: 2024/10/23 20:18:27 by ahaddadi         ###   ########.fr       */
+/*   Updated: 2024/10/25 16:19:14 by ahaddadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int ft_isdigit(int d)
 {
-	if (d >= 0 && d <= 9)
-		return (1);
-	return (0);
+	return (d >= '0' && d <= '9');
+	// 	return (1);
+	// return (0);
 }
