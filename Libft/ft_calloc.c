bc216b3d18db178917e6cd67d8a2d02afddd998a
mhdd <em>
2024-10-25 17:34:48 +0100

    calloc fix bzero

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -1,20 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahaddadi <ahaddadi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/24 19:27:40 by ahaddadi          #+#    #+#             */
-/*   Updated: 2024/10/25 17:15:05 by ahaddadi         ###   ########.fr       */
+/*   Created: 2024/10/25 16:39:40 by ahaddadi          #+#    #+#             */
+/*   Updated: 2024/10/25 17:32:59 by ahaddadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_bzero(void *s, size_t n)
+#include <stdlib.h>
+#include <stddef.h>
+
+void *ft_calloc(size_t count, size_t size)
 {
-	if (n != 0)
-	{
-		while (n-- > 0)
-			*s++ = 0;
-	}
+	if(cout == 0 || size ==0)
+		return malloc(1);
+	void *space = malloc(count * size);
+	if (!space)
+		return NULL;
+	ft_bzero(space, count * size);
+	return space;
 }
