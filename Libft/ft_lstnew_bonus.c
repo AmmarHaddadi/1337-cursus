2dd3c65627a55ba45f787bff15777f76f2339571
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2024-11-03 12:50:53 +0100

    lst new fixed

--- Libft/ft_lstnew_bonus.c
+++ Libft/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ahaddadi <ahaddadi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 15:14:09 by ahaddadi          #+#    #+#             */
-/*   Updated: 2024/11/01 11:14:05 by ahaddadi         ###   ########.fr       */
+/*   Updated: 2024/11/03 12:50:19 by ahaddadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,12 @@
 t_list *ft_lstnew(void *content)
 {
 	t_list *new = malloc(sizeof(t_list));
-	new->content = content;
+	if (!new)
+		return NULL;
+	if(content)
+		new->content = content;
+	else if (!content)
+		new->content = NULL;
 	new->next = NULL;
 	return new;
 }
