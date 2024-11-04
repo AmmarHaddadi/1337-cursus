d06d552035552a91e3dc4460f51a5e552ae191a4
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2024-11-04 11:24:51 +0100

    memcpy now like original

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ahaddadi <ahaddadi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 20:01:39 by ahaddadi          #+#    #+#             */
-/*   Updated: 2024/10/25 08:16:05 by ahaddadi         ###   ########.fr       */
+/*   Updated: 2024/11/04 11:24:38 by ahaddadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 // talamis
 void * ft_memcpy(void * dst, const void * src, size_t n)
 {
+	if (dst == src)
+		return dst;
 	unsigned char *ori = dst;
 	while (n-- > 0)
 		*(unsigned char *)dst++ = *(unsigned char *)src++;
@@ -22,11 +24,11 @@ void * ft_memcpy(void * dst, const void * src, size_t n)
 }
 
 // #include <stdio.h>
-
+// #include <string.h>
 // int main()
 // {
-// 	char s[] = "Ammar";
-// 	char d[6];
-// 	printf("%s", ft_memcpy(d, s+2, sizeof(char) * 3));
+// 	char *s = "Ammar";
+// 	printf("%s", ft_memcpy(s, s, 5));
+// 	// printf("%s", memcpy(s, s, 5));
 // }
 
