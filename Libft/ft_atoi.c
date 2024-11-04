3994fcf4535ac00716c9d7e7d8e95c1684fa1ae9
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2024-11-04 11:58:50 +0100

    +test unit atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ahaddadi <ahaddadi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 19:46:07 by ahaddadi          #+#    #+#             */
-/*   Updated: 2024/10/25 16:37:55 by ahaddadi         ###   ########.fr       */
+/*   Updated: 2024/11/04 11:58:15 by ahaddadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,3 +37,47 @@ int ft_atoi(const char *str)
 // 	printf("%d", ft_atoi("123"));
 // }
 
+// #include <stdio.h>
+// #include <stdlib.h> // For the original atoi
+// #include <limits.h>
+// #include "libft.h" // Assuming ft_atoi is declared in libft.h
+
+// void test_atoi_functions(const char *str)
+// {
+// 	int result_ft_atoi = ft_atoi(str);
+// 	int result_atoi = atoi(str);
+
+// 	if (result_ft_atoi == result_atoi)
+// 		printf("PASS: ft_atoi(\"%s\") = %d, atoi(\"%s\") = %d\n", str, result_ft_atoi, str, result_atoi);
+// 	else
+// 		printf("FAIL: ft_atoi(\"%s\") = %d, atoi(\"%s\") = %d\n", str, result_ft_atoi, str, result_atoi);
+// }
+
+// int main()
+// {
+// 	// Basic tests
+// 	test_atoi_functions("123");
+// 	test_atoi_functions("-123");
+// 	test_atoi_functions("+123");
+// 	test_atoi_functions("0");
+
+// 	// Tests with leading spaces
+// 	test_atoi_functions("   123");
+// 	test_atoi_functions("   -123");
+// 	test_atoi_functions("   +123");
+
+// 	// Tests with non-digit characters
+// 	test_atoi_functions("123abc");
+// 	test_atoi_functions("-123abc");
+// 	test_atoi_functions("+123abc");
+
+// 	// Edge cases
+// 	test_atoi_functions("2147483647"); // INT_MAX
+// 	test_atoi_functions("-2147483648"); // INT_MIN
+
+// 	// Overflow cases (behavior may vary depending on implementation)
+// 	test_atoi_functions("2147483648"); // INT_MAX + 1
+// 	test_atoi_functions("-2147483649"); // INT_MIN - 1
+
+// 	return 0;
+// }
